50f93fc61ec75be002f19b3f68cf491479dd818f
Luka Hietala <95122845+LukaHietala@users.noreply.github.com>
2023-09-09 11:54:19 +0000

    fix vercel issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind()],
-  output: "hybrid",
+  output: "static",
   adapter: vercel({ analytics: true })
 });
