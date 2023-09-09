638d4f3ea50f5adcf8d4b4cfd72c9e96ab6d6eec
Luka Hietala <95122845+LukaHietala@users.noreply.github.com>
2023-09-09 18:09:42 +0000

    test 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/static";
+import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), sitemap(), tailwind()],
   output: "static",
-  adapter: vercel()
+  adapter: vercelStatic(),
 });
