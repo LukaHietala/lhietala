9e0d6f4250f6fb909cdb168c0b9b83feb51ea840
Luka Hietala <95122845+LukaHietala@users.noreply.github.com>
2023-09-09 11:49:03 +0000

    add ability to view posts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,24 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ["system-ui",
+					"-apple-system",
+					"BlinkMacSystemFont",
+					"Segoe UI",
+					"Roboto",
+					"Oxygen",
+					"Ubuntu",
+					"Cantarell",
+					"Open Sans",
+					"Helvetica Neue",
+					"sans-serif"],
+		}
+		},
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
