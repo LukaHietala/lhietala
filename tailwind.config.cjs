34130ca291f425e84722d6f2b195fa1738330513
Luka Hietala <95122845+LukaHietala@users.noreply.github.com>
2023-09-10 14:46:33 +0000

    styling changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,17 +6,7 @@ module.exports = {
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: ["system-ui",
-					"-apple-system",
-					"BlinkMacSystemFont",
-					"Segoe UI",
-					"Roboto",
-					"Oxygen",
-					"Ubuntu",
-					"Cantarell",
-					"Open Sans",
-					"Helvetica Neue",
-					"sans-serif"],
+				sans: ["Inter", ...defaultTheme.fontFamily.sans],
 		}
 		},
 	},
